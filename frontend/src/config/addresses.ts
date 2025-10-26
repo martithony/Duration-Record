26a95bbd7bf7da63d33495c29b6edc91d2dffd96
rinhuhimira <moherleipheimer@gmail.com>
2025-10-26 18:36:44 +0800

    add config addresses

---
+++ frontend/src/config/addresses.ts
@@ -0,0 +1,7 @@
+export const CONTRACT_ADDRESSES: Record<string, { address: `0x${string}`; chainId: number; chainName: string }> = {
+  "11155111": { 
+    address: "0x4289e3AfB1eEe311347A4A6d4Fb34782b8F27c02" as `0x${string}`, 
+    chainId: 11155111, 
+    chainName: "Sepolia" 
+  },
+};
